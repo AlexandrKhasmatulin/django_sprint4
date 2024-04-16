4f4252382ebdc9ac9a1be4b1bd0daeaf6be34b93
haa <alexandr.khasmatulin@yandex.ru>
2024-04-16 22:52:27 +0300

    final

--- blogicum/blog/_test.py
+++
@@ -1,9 +0,0 @@
-class PostListView(ListView):
-    model = Category
-    form_class = CategoryForm
-    template_name = 'blog/index.html'
-    ordering = 'id'
-
-    def get_success_url(self):
-        return reverse('blog:profile',
-                       kwargs={'username': self.request.user.username})
